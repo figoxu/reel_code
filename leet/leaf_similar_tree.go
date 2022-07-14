e259c88151421a0ead73e01122af05ca47075a58
xujh <xujianhui@xxbmm.com>
2022-07-14 14:23:58 +0800

    minor init leaf-similar-trees

---
+++ leet/leaf_similar_tree.go
@@ -0,0 +1,99 @@
+package main
+
+import "fmt"
+
+//https://leetcode.cn/problems/leaf-similar-trees/
+func main() {
+	tree1 := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 7,
+				},
+				Right: &TreeNode{
+					Val: 4,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val: 9,
+			},
+			Right: &TreeNode{
+				Val: 8,
+			},
+		},
+	}
+	tree2 := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+		Right: &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 9,
+				},
+				Right: &TreeNode{
+					Val: 8,
+				},
+			},
+		},
+	}
+	similarFlag := leafSimilar(tree1, tree2)
+	fmt.Println(similarFlag)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	list1 := make([]int, 0)
+	list2 := make([]int, 0)
+	getLeaf(&list1, root1)
+	getLeaf(&list2, root2)
+	if len(list1) != len(list2) {
+		return false
+	}
+
+	for i := 0; i < len(list1); i++ {
+		if list1[i] != list2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func getLeaf(list *[]int, root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	if root.Left == nil && root.Right == nil {
+		*list = append(*list, root.Val)
+	} else {
+		getLeaf(list, root.Left)
+		getLeaf(list, root.Right)
+	}
+}
